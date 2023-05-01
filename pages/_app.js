f121f08fc4053ed33b394acd2636ffa79e882374
frdrk00 <masterkooo@gmail.com>
2023-05-01 14:57:50 +0200

    single product page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import { CartContextProvider } from "@/components/CartContext";
 import { createGlobalStyle } from "styled-components"
+import "../styles/Global.css"
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
   body{
     background-color: #eee;
     padding:0;
